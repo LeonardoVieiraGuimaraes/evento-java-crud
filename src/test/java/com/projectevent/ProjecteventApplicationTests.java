fa316962c6795d63529b35a24308ccdc871fc56f
LEONARDO VIEIRA GUIMARAES <42416757+LeonardoVieiraGuimaraes@users.noreply.github.com>
2022-11-10 08:25:40 -0300

    inicio

---
+++ src/test/java/com/projectevent/ProjecteventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecteventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
