b06ae70e85c2e9f8ec9b2f5365657c57362d19e6
LEONARDO VIEIRA GUIMARAES <42416757+LeonardoVieiraGuimaraes@users.noreply.github.com>
2022-11-10 10:58:20 -0300

    Update ProjecteventApplication.java

--- src/main/java/com/projectevent/ProjecteventApplication.java
+++ src/main/java/com/projectevent/ProjecteventApplication.java
@@ -2,8 +2,10 @@ package com.projectevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class ProjecteventApplication {
 
 	public static void main(String[] args) {
