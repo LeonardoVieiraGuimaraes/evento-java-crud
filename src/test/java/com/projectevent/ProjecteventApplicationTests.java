dbdea4567829afb0cd8e06b328e3db4f7faa4c64
LEONARDO VIEIRA GUIMARAES <42416757+LeonardoVieiraGuimaraes@users.noreply.github.com>
2022-11-13 11:54:04 -0300

    Update ProjecteventApplicationTests.java

--- src/test/java/com/projectevent/ProjecteventApplicationTests.java
+++ src/test/java/com/projectevent/ProjecteventApplicationTests.java
@@ -3,7 +3,7 @@ package com.projectevent;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProjecteventApplicationTests.class)
 class ProjecteventApplicationTests {
 
 	@Test
